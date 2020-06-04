a4a09cf2bc445a750c2460c0c8b41952b1d7453f
sridhar-shelge <66238344+sridhar-shelge@users.noreply.github.com>
2020-06-04 11:56:58 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=8889;dbname=mentormind', 
-   'root', 'root');
+$pdo = new PDO('mysql:host=sql12.freemysqlhosting.net;port=3306;dbname=sql12345260', 
+   'sql12345260', 'DnyRULRXLK');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
