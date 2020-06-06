caee6713e5f5922d5bfc76b925a7bc1e065faba0
sridhar-shelge <66238344+sridhar-shelge@users.noreply.github.com>
2020-06-06 14:49:21 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,5 +1,9 @@
 <?php
+env:
+  username: ${{ username }}
+  password: ${{ password }}
+
 $pdo = new PDO('mysql:host=sql12.freemysqlhosting.net;port=3306;dbname=sql12345260', 
-   'sql12345260', 'DnyRULRXLK');
+   'username', 'password');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
