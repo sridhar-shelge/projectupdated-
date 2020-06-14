9719273782b56c91b19dede8c92404b3c7e249d9
sridhar-shelge <66238344+sridhar-shelge@users.noreply.github.com>
2020-06-14 11:33:56 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,6 +1,6 @@
 <?php
 
-$pdo = new PDO('mysql:host=https://databases.000webhost.com;dbname=id14057223_mydatabase', 
+$pdo = new PDO('mysql:host=databases.000webhost.com;dbname=id14057223_mydatabase', 
    'id14057223_sridhar', 'qA/$nMnIR>J2dram');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
