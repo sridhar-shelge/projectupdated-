3cab3f78390faacf1b8a74452c91c60c8ac430a9
sridhar-shelge <66238344+sridhar-shelge@users.noreply.github.com>
2020-06-18 15:07:17 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,6 +1,6 @@
 <?php
 
-$pdo = new PDO('mysql:host=65.19.141.67;port=3306;dbname=sridhar7_mentormind', 
+$pdo = new PDO('mysql:host=sridhar7.heliohost.org;port=3306;dbname=sridhar7_mentormind', 
    'sridhar7_sridhar', 'mysql@122410');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
