701e9f1c143dfd32f3ed04203e8b4536841fef29
sridhar-shelge <66238344+sridhar-shelge@users.noreply.github.com>
2020-06-18 17:36:56 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,4 +1,5 @@
 <?php
+
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
@@ -6,5 +7,10 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-$conn = new mysqli($server, $username, $password, $db);
+$pdo = new PDO("mysql:host=$server;port=8889;dbname=$db", 
+   $username, $password);
+
+// See the "errors" folder for details...
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 ?>
